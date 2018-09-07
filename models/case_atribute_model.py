395a13c8778095c03cf84ab534797fca84cffd58
Guilherme Henschel <guilherme.g.henschel@gmail.com>
2018-09-06 22:40:20 -0300

    Decision Tree Implementation v0.1

--- models/case_atribute_model.py
+++ models/case_atribute_model.py
@@ -1,6 +1,36 @@
 from PyQt5.QtCore import qDebug
 import csv
 from models.case_model import CaseModel
+from sklearn import tree, model_selection, metrics, preprocessing
+import numpy as np
+
+
+def tree_to_code(clf, feature_names, class_names):
+    tree_ = clf.tree_
+    string = ""
+    feature_name = [
+        feature_names[i] if i != tree._tree.TREE_UNDEFINED else "undefined!"
+        for i in tree_.feature
+    ]
+    string+="def tree({}):\n".format(", ".join(feature_names))
+
+    def recurse(node, depth):
+        indent = "  " * depth
+        stri = ""
+        if tree_.feature[node] != tree._tree.TREE_UNDEFINED:
+            name = feature_name[node]
+            threshold = tree_.threshold[node]
+            stri +="{}if {} <= {}:\n".format(indent, name, threshold)
+            stri +=recurse(tree_.children_left[node], depth + 1)
+            stri +="{}else:  # if {} > {}\n".format(indent, name, threshold)
+            stri +=recurse(tree_.children_right[node], depth + 1)
+        else:
+            stri +="{}return {}\n".format(indent, class_names[np.argmax(tree_.value[node][0])])
+        return stri
+
+    string+=recurse(0, 1)
+
+    return string
 
 class CaseAttributeModel(CaseModel):
     def details(self):
@@ -59,4 +89,72 @@ class CaseAttributeModel(CaseModel):
                 writer.writerow(l)
                 n+=1
 
+
+
+    def generate_tree(self,attribute_types,params):
+        index_of_class = params["class_index"]
+        case_is_attribute = params["id_is_class"]
+        perct_of_test = params["perct_of_test"]
+        list_of_ignored_attributes = params["ignored_attributes"]
+
+
+        #Start Slicing
+        data = []
+        target = []
+        list_of_strings = []
+        feature_names = []
+        class_name = []
+        if case_is_attribute:
+            feature_names.append("Case")
+
+
+        for case, attr in self.cases.items():
+            attributes = []
+            if case_is_attribute:
+                attributes.append(str(case))
+            for i in range(0, len(attr[0])):
+                #Type Conversion
+                if not i in list_of_ignored_attributes:
+                    if attribute_types[i]== 0 and i != index_of_class:
+                        value = bool(attr[0][i])
+                    elif (attribute_types[i]== 1 or attribute_types[i]== 2)  and i != index_of_class:
+                        value = attr[0][i]
+                    else:
+                        if i == index_of_class:
+                            if str(attr[0][i]) in list_of_strings:
+                                value = list_of_strings.index(str(attr[0][i]))
+                            else:
+                                list_of_strings.append(attr[0][i])
+                                value = list_of_strings.index(str(attr[0][i]))
+                        else:
+                            #AVISAR ABORT
+                            list_of_ignored_attributes.append(i)
+                            return ;
+
+                    if i != index_of_class:
+                        attributes.append(value)
+                    else:
+                        target.append(value)
+            data.append(attributes)
+
+        #legend
+        for item in range(1,len(self.legend)):
+            if item-1!=index_of_class and not item-1 in list_of_ignored_attributes:
+                feature_names.append(self.legend[item])
+            elif not item-1 in list_of_ignored_attributes:
+                class_name.append(self.legend[item])
+
+        #training data e test data
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(data,target, test_size = perct_of_test, random_state=100)
+
+        clf = tree.DecisionTreeClassifier(criterion = params["criterion"],splitter=params["splitter"],max_depth=params["max_depth"],
+                                          min_samples_split=params["min_sample_split"],min_samples_leaf=params["min_sample_leafs"],
+                                          min_weight_fraction_leaf = params["min_weight_fraction_leaf"],max_features=params["max_features"],
+                                          random_state = params["random_state"],max_leaf_nodes=params["max_leaf_nodes"],
+                                          min_impurity_split=params["min_impurity_split"])
+
+        clf.fit(X_train,y_train)
+
+        code = tree_to_code(clf,feature_names,list_of_strings)
+        
 
--- unitTest/case_event_model_import.py
+++ unitTest/case_event_model_import.py
@@ -1,7 +1,27 @@
 from models.case_atribute_model import CaseAttributeModel
 
+
 c = CaseAttributeModel()
 
-c.create_from_file("../Data/my_atributes.csv",";")
+c.create_from_file("../Data/AttrLog.csv",";")
 
-print(c.details())
+#print(c.details())
+
+params = {"class_index": 169,
+          "id_is_class": False,
+          "perct_of_test": 0.3,
+          "criterion":"gini",
+          "splitter": "best",
+          "max_features": None,
+          "max_depth": 10,
+          "min_sample_split": 2,
+          "min_sample_leafs": 1,
+          "min_weight_fraction_leaf": 0.0,
+          "random_state": None,
+          "max_leaf_nodes": None,
+          "min_impurity_split": None,
+          "ignored_attributes": [167,168]
+          }
+attribute_types = [1 for i in range(0,len(c.legend)-1)]
+attribute_types.append(3)
+c.generate_tree(attribute_types,params)
